92ed3530fb8526aeb199f5a544e37dfc9b1c24ff
Ivan <ivanclair0710@gmail.com>
2020-10-29 00:33:56 +0800

    import dependency injection

--- app/src/main/java/ivan/personal/mypractice/MainActivity.kt
+++ app/src/main/java/ivan/personal/mypractice/MainActivity.kt
@@ -2,10 +2,17 @@ package ivan.personal.mypractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    // region Life Cycle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    // endregion
 }
---
+++ app/src/main/java/ivan/personal/mypractice/MainApplication.kt
@@ -0,0 +1,7 @@
+package ivan.personal.mypractice
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MainApplication : Application()
